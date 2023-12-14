3ad6217de34cb9b03351c520edcc11cc105b945a
PAWAN SINGH <pawansingh55678@gmail.com>
2023-12-14 18:04:20 +0530

    initial commit

---
+++ javas.js
@@ -0,0 +1,24 @@
+let arrayList = [];
+function addList(){
+    const input = document.querySelector('.input');
+    const dueDate = document.querySelector('.js-todo');
+    arrayList.push({todo : input.value, duedate : dueDate.value}); 
+    let HTMLstring = '';
+    for(let i = 0; i < arrayList.length; i++){
+        HTMLstring += `<p>${arrayList[i].todo} ${arrayList[i].duedate} <button onclick="delet(${i})">Delete</button></p>`;
+    }
+    const div = document.querySelector('div');
+    div.innerHTML = HTMLstring;
+    input.value = "";
+    dueDate.value = "";
+}
+function delet(val){
+    arrayList.splice(val,1);
+    console.log(arrayList);
+    let HTMLstring = '';
+    for(let i = 0; i < arrayList.length; i++){
+        HTMLstring += `<p>${arrayList[i].todo} ${arrayList[i].duedate} <button onclick="delet(${i})">Delete</button></p>`;
+    }
+    div = document.querySelector('div');
+    div.innerHTML = HTMLstring;
+}
