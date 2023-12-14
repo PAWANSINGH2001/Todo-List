eb0bf9986b3ba5464c2ac279f942267e87e3e6ba
PAWAN SINGH <pawansingh55678@gmail.com>
2023-12-14 18:10:13 +0530

    fixed issues

--- javas.js
+++ javas.js
@@ -2,15 +2,17 @@ let arrayList = [];
 function addList(){
     const input = document.querySelector('.input');
     const dueDate = document.querySelector('.js-todo');
-    arrayList.push({todo : input.value, duedate : dueDate.value}); 
-    let HTMLstring = '';
-    for(let i = 0; i < arrayList.length; i++){
-        HTMLstring += `<p>${arrayList[i].todo} ${arrayList[i].duedate} <button onclick="delet(${i})">Delete</button></p>`;
+    if(input.value !== "" && dueDate.value !== ""){
+        arrayList.push({todo : input.value,duedate : dueDate.value}); 
+        let HTMLstring = '';
+        for(let i = 0; i < arrayList.length; i++){
+            HTMLstring += `<p>${arrayList[i].todo} ${arrayList[i].duedate} <button onclick="delet(${i})">Delete</button></p>`;
+        }
+        const div = document.querySelector('div');
+        div.innerHTML = HTMLstring;
+        input.value = "";
+        dueDate.value = "";
     }
-    const div = document.querySelector('div');
-    div.innerHTML = HTMLstring;
-    input.value = "";
-    dueDate.value = "";
 }
 function delet(val){
     arrayList.splice(val,1);
